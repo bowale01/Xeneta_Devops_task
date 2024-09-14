3576365c8aefb3b28b23d5bb4279ce9e3f3f1c80
A.A <37187773+bowale01@users.noreply.github.com>
2024-09-14 22:14:31 +0200

    Update rates.sql

--- db/rates.sql
+++ db/rates.sql
@@ -1,1 +1,15 @@
-l
+-- rates.sql
+
+CREATE TABLE rates (
+    id SERIAL PRIMARY KEY,
+    day DATE NOT NULL,
+    orig_code VARCHAR(10),
+    dest_code VARCHAR(10),
+    price NUMERIC,
+    count INTEGER
+);
+
+INSERT INTO rates (day, orig_code, dest_code, price, count) VALUES 
+('2021-01-31', 'CNGGZ', 'EETLL', 1154.33, 3),
+('2021-01-30', 'CNGGZ', 'EETLL', 1154.33, 3),
+('2021-01-29', 'CNGGZ', 'EETLL', 1154.33, 3);
